1ccba2e1064f7a06ceadd9745977988a93c14f8f
kolladarshan <kolladarshan21@gmail.com>
2026-02-12 08:24:23 +0530

    CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from 'src/dto/create-user';
+import { UpdateUserDto } from 'src/dto/update-user';
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
 
-@Controller('user')
-export class UserController {}
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
+}
